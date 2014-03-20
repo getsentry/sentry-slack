8aa6d501bcde8d0fe6c8b9771715e3455bc1e937
Matt Robenolt <matt@ydekproductions.com>
2014-03-20 02:06:52 -0700

    0.1.0

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ install_requires = [
 
 setup(
     name='sentry-slack',
-    version='0.0.0',
+    version='0.1.0',
     author='Matt Robenolt',
     author_email='matt@ydekproductons.com',
     url='https://github.com/getsentry/sentry-slack',
