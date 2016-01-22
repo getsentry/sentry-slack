2acb9359f8858778356d0d62182e2f74310ef02e
David Cramer <dcramer@gmail.com>
2016-01-22 12:44:58 -0800

    Expand test

--- tests/sentry_slack/test_plugin.py
+++ tests/sentry_slack/test_plugin.py
@@ -6,6 +6,8 @@ from exam import fixture
 from sentry.models import Rule
 from sentry.plugins import Notification
 from sentry.testutils import TestCase
+from sentry.utils import json
+from urlparse import parse_qs
 
 from sentry_slack.plugin import SlackPlugin
 
@@ -29,3 +31,31 @@ class SlackPluginTest(TestCase):
 
         with self.options({'system.url-prefix': 'http://example.com'}):
             self.plugin.notify(notification)
+
+        request = responses.calls[0].request
+        payload = json.loads(parse_qs(request.body)['payload'][0])
+        assert payload == {
+            'parse': 'none',
+            'username': 'Sentry',
+            'attachments': [
+                {
+                    'color': '#f43f20',
+                    'fields': [
+                        {
+                            'short': False,
+                            'value': 'foo.bar',
+                            'title': 'Culprit',
+                        },
+                        {
+                            'short': True,
+                            'value': 'foo Bar',
+                            'title': 'Project'
+                        },
+                    ],
+                    'fallback': '[foo Bar] Hello world',
+                    'title': 'Hello world',
+                    'title_link': 'http://example.com/baz/bar/issues/1/',
+                },
+            ],
+        }
+
